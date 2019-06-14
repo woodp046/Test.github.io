7ec54733a5da1b6db6dc5faf6b73945c2c440316
woodp046 <51137926+woodp046@users.noreply.github.com>
2019-06-14 10:17:52 -0700

    Update starter.js

--- starter.js
+++ starter.js
@@ -1,4 +1,8 @@
 var starter = ["It was", "They were", "The night", "When the", "We were", "When the"];
-function generate() {
+var topic = ["Earth", "Love", "Fighting", "Magic", "Politics", "Food", "Animals", "Stress", "Adventure"];
+function generateStarterWord() {
 	document.getElementById("generate").innerHTML = starter[parseInt(Math.random() * 5)];
+};
+function generateTopic() {
+	document.getElementById("generateTopic").innerHTML = topic[parseInt(Math.random() * 8)];
 };
